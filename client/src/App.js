b359aa324732502e42c6313a94f5e65344d054c2
joseph mwanzia <makamwanzia@gmail.com>
2024-05-29 11:57:15 +0300

    registration, login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Login from './componets/Login';
+import Registration from './componets/registration/Registration';
+import Candidates from './componets/Candidates';
 
 function App() {
+  const [companyUser, setCompanyUser] = useState()
+
+  useEffect(() => {
+    fetch('/me')
+    .then(res => {if (res.ok) {
+      res.json().then(data => { setCompanyUser(data)})}
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/login' element={<Login companyUser={companyUser}/>}/>
+        <Route path='/registration' element={<Registration/> }/>
+        <Route path='/candidates' element={<Candidates companyUser={companyUser}/>}/>
+      </Routes>
     </div>
   );
 }
