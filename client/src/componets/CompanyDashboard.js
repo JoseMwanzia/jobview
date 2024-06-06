5898b6422ec7ad7653214757de3e4e61c9a58cf1
joseph mwanzia <makamwanzia@gmail.com>
2024-06-06 23:46:43 +0300

    homepage design

--- client/src/componets/CompanyDashboard.js
+++ client/src/componets/CompanyDashboard.js
@@ -6,57 +6,68 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
+import LandingPage from './LandingPage';
 
 
 function CompanyDashboard() {
   return (
     <div style={{height: '1000px'}}>
-      <Navbar expand="lg"  className="bg-body-tertiary">
+      <Navbar expand="lg" collapseOnSelect sticky='top' bg="dark" data-bs-theme="dark"  className="bg-body-tertiary">
         <Container fluid style={{width: "100%"}}>
+
           <Col xs={6} md={4}>
-            <Image src={logo} rounded style={{width: "25%", aspectRatio: "3/2", objectFit: 'contain'}} />
+            <Link to='/companyDashboard'>
+              <Image src={logo} rounded style={{width: "25%", aspectRatio: "3/2", objectFit: 'contain'}} />
+            </Link>
           </Col>
           <Navbar.Toggle aria-controls="navbarScroll" />
+
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
+              // style={{ maxHeight: '100px' }}
               navbarScroll
             >
               <Nav.Link href="#action1">Home</Nav.Link>
               <Nav.Link href="#action2">Find Jobs</Nav.Link>
+
               <NavDropdown title="Employers" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Find Talent</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
+                <NavDropdown.Item href="#find-talent">Find Talent</NavDropdown.Item>
+                <NavDropdown.Item href="#more-talent">
                   More Talent
                 </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
+                {/* <NavDropdown.Divider /> */}
+                <NavDropdown.Item href="#more-talent2">
                 More Talent
                 </NavDropdown.Item>
               </NavDropdown>
+
               <NavDropdown title="Candidates" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Job Listings</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">
                 More Talent
                 </NavDropdown.Item>
-                <NavDropdown.Divider />
+                {/* <NavDropdown.Divider /> */}
                 <NavDropdown.Item href="#action5">
                   Job Listings
                 </NavDropdown.Item>
               </NavDropdown>
+
               <NavDropdown title="Pages" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">
                   Another action
                 </NavDropdown.Item>
-                <NavDropdown.Divider />
+                {/* <NavDropdown.Divider /> */}
                 <NavDropdown.Item href="#action5">
                   Something else here
                 </NavDropdown.Item>
               </NavDropdown>
+
             </Nav>
             <Form className="d-flex">
               <Form.Control
@@ -70,6 +81,8 @@ function CompanyDashboard() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <LandingPage/>
     </div>
   )
 }
