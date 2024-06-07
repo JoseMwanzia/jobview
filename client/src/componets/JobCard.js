28f30bc73347962d3922881da7cf074ff509dca1
joseph mwanzia <makamwanzia@gmail.com>
2024-06-07 04:00:55 +0300

    format

--- client/src/componets/JobCard.js
+++ client/src/componets/JobCard.js
@@ -14,33 +14,33 @@ useEffect(() => {
 }, [])
     
 if (!companies || companies.length === 0) {
-  return (<div className="text-center">
-    <div className="spinner-border" role="status">
-      <span className="sr-only"></span>
-    </div>
-  </div>);
-}
-
   return (
-      <>
-      {companies.map((company) => {
-    return (
-      <Card key={company.id} className="text-center col-lg-6 col-md-6 mb-4">
-      <Card.Header>{company.company_name}</Card.Header>
-      <Card.Body>
-        <Card.Title>{company.email}</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      <Card.Footer className="text-muted">{formatDistanceToNow(parseISO(company.created_at))} ago</Card.Footer>
-    </Card>
+    <div className="text-center">
+        <div className="spinner-border" role="status">
+          <span className="sr-only"></span>
+        </div>
+    </div>);
+} else {
+  return (
+    <>
+        {companies.map((company) => {
+          return (
+            <Card key={company.id} className="text-center col-lg-6 col-md-6 mb-4">
+            <Card.Header>{company.company_name}</Card.Header>
+            <Card.Body>
+              <Card.Title>{company.email}</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">{formatDistanceToNow(parseISO(company.created_at))} ago</Card.Footer>
+            </Card>
+          )
+      })}
+    </>
     )
-  })}
-      </>
-
-  );
+  }
 }
 
 export default JobCard;
