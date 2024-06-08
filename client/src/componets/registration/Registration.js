75260699a6fa8dfc8206e2ba37707b4a13cf9e4a
joseph mwanzia <makamwanzia@gmail.com>
2024-06-08 23:55:47 +0300

    logout button added

--- client/src/componets/registration/Registration.js
+++ client/src/componets/registration/Registration.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Logout from "../Logout";
+import Logout from "../LoginAndLogout/Logout";
 
 function Registration() {
     
