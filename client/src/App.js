3e4e19f037d6cca69eb5577c16035725934c7202
joseph mwanzia <makamwanzia@gmail.com>
2024-06-08 23:59:46 +0300

    js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,58 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
-import Login from './componets/Login';
+import Login from './componets/LoginAndLogout/Login';
 import Registration from './componets/registration/Registration';
-import CompanyDashboard from './componets/CompanyDashboard';
+import CompanyDashboard from './componets/company/CompanyDashboard';
+import Example from './Example';
+import JobSeekerDashboard from './componets/jobSeeker/JobSeekerDashboard';
+import LoginJobSeeker from './componets/LoginAndLogout/LoginJobSeeker';
 
 function App() {
-  const [companyUser, setCompanyUser] = useState()
+  const [companyUser, setCompanyUser] = useState([])
+  const [myCompany, setMyCompany] = useState([])
+  const [jobSeeker, setJobSeeker] = useState([])
 
   useEffect(() => {
-    // keep logged in
-    fetch('/me')
+    // all companies to jobseeker
+    fetch('/allCompanies')
     .then(res => {if (res.ok) {
       res.json().then(data => { setCompanyUser(data)})}
     })
   }, [])
+  // console.log(companyUser);
 
-  if (!companyUser) {
-    return <Login/>
+  useEffect(() => {
+    // keep company logged in
+    const api = async () => {
+      const response = await fetch('/me')
+      const data = await response.json()
+      setMyCompany(data);
+    }
+    api()
+  }, [])
+
+  useEffect(() => {
+    // keep jobseeker logged in
+    fetch('/me_jobseeker')
+    .then(res => {if (res.ok) {
+      res.json().then(data => { setJobSeeker(data)})}
+    })
+  }, [])
+
+  if (!jobSeeker) {
+    return <LoginJobSeeker/>
   }
   else {
     return (
       <div className='App'>
         <Routes>
-          {/* <Route path='/' element={</>}/> */}
-          <Route path='/login' element={<Login/>}/>
           <Route path='/registration' element={<Registration/> }/>
-          <Route path='/companyDashboard' element={<CompanyDashboard companyUser={companyUser}/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/loginJobSeeker' element={<LoginJobSeeker/>}/>
+          <Route path='/companyDashboard' element={<CompanyDashboard myCompany={myCompany}/>}/>
+          <Route path='/jobSeekerDashboard' element={<JobSeekerDashboard jobSeeker={jobSeeker} companyUser={companyUser}/>}/>
+          <Route path='/example' element={<Example/>}/>
         </Routes>
       </div>
     )
