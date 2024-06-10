07f783d2ec773dbd336bd963bb4471125adb2128
joseph mwanzia <makamwanzia@gmail.com>
2024-06-10 23:39:32 +0300

    routes and active-records migrations files

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,35 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post '/company', to: 'companies#create'
+  post '/applications', to: 'applications#create'
   get '/companies', to: 'companies#index'
+  get '/applicableCompanies', to: 'job_seekers#companies'
   get '/company/:id', to: 'companies#show'
-  get '/me', to: 'companies#show'
+  get '/my_company', to: 'companies#show'
+  get '/jobseeker', to: 'job_seekers#show'
   patch '/company/:id', to: 'companies#update'
   delete '/company/:id', to: 'companies#destroy'
   
   
   post '/me_company', to: 'sessions#create'
-  get '/me_sekeer', to: 'sessions#create_seeker'
+  post '/me_sekeer', to: 'sessions#create_seeker'
   delete '/logout', to: 'sessions#destroy_company'
+  delete '/logout_jobseeker', to: 'sessions#destroy_jobseeker'
+  
   
   get '/job_seeker', to: 'job_seekers#index'
   get '/job_seeker/:id', to: 'job_seekers#show'
   post '/job_seeker', to: 'job_seekers#create'
+
+  resources :job_seekers do
+    resources :applications, only: [:index, :create]
+  end
+
+  # resources :companies do
+  #   resources :applications, only: [:index, :create, :show]
+  # end
+
+  post '/companies/:company_id/applications', to: 'applications#create'
+
+  resources :applications, only: [:index, :show, :destroy]
 end
