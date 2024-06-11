cdbe2ca529583e243179650ec765231892566725
joseph mwanzia <makamwanzia@gmail.com>
2024-06-11 23:22:21 +0300

    resume serialization

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,5 +1,6 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :address, :phone, :job_seeker_id, :company_id, :created_at, :updated_at, :resume
+  attributes :id, :name, :created_at, :resume_url
+  # attributes :id, :name, :email, :address, :phone, :job_seeker_id, :company_id, :created_at, :updated_at, :resume_url
 
   belongs_to :job_seeker
   belongs_to :company
