eea3158ba1bca37d8a64b52d682182d5364aa223
joseph mwanzia <makamwanzia@gmail.com>
2024-06-11 23:19:19 +0300

    resume url using Active-Record

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,10 @@ class Application < ApplicationRecord
   belongs_to :job_seeker
   belongs_to :company
   
-  has_one_attached :resume
+  has_one_attached :resume, dependent: :destroy
+  def resume_url
+    Rails.application.routes.url_helpers.url_for(resume) if resume.attached?
+  end
 
   validates :name, :email, :address, :phone, presence: true
 
