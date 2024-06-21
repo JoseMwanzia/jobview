05c53e5407972cc325b2318e4dd3664fbdf8ba15
joseph mwanzia <makamwanzia@gmail.com>
2024-06-21 23:53:07 +0300

    company serializer association for applications

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -2,6 +2,6 @@ class CompanySerializer < ActiveModel::Serializer
   attributes :id, :company_name, :email, :created_at
 
   has_many :posts
-  # has_many :applications
+  has_many :applications
   has_many :job_seekers, through: :applications
 end
