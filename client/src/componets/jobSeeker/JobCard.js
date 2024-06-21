621b20fa8ca7c88a59aaa28e733f5b7df724db95
joseph mwanzia <makamwanzia@gmail.com>
2024-06-21 23:39:16 +0300

    code refactored

--- client/src/componets/jobSeeker/JobCard.js
+++ client/src/componets/jobSeeker/JobCard.js
@@ -3,50 +3,42 @@ import Card from 'react-bootstrap/Card';
 import { formatDistanceToNow, parseISO } from 'date-fns';
 
 import ApplicationsModal from './ApplicationsModal';
+  const companyPost = companyUser.map((company) =>  (company.posts.map((post) => {
 
-function JobCard({jobSeeker, companyUser, loading}) {
-  // const [job_seeker, setJob_seeker] = useState([jobSeeker])
-  // const [companies, setCompanies] = useState(companyUser)
-  // console.log(companyUser);
-
-// useEffect(() => {
-//   fetch(`/companies`)
-//     .then(res => res.json())
-//     .then(data => setCompanies(data))
-//     .catch(error => console.error(error))
-// }, [])
     
-if (!jobSeeker || jobSeeker.length === 0) {
-  return (
-    <div className="text-center">
-        <div className="spinner-border" role="status">
-          <span className="sr-only"></span>
-        </div>
-    </div>);
-} else {
-  return (
-    <>
-        {companyUser.map((company) => {
-        //   console.log(company.company_name);
-          return (
-            <Card key={company.id} className="text-center col-lg-6 col-md-6 mb-4 crd">
+
+        return (
+          <Card key={post.id} className={`text-center col-lg-6 col-md-5 mb-4 mx-2 crd ${open === post.id ? 'border-3 border-primary' : '' }`}>
             <Card.Header>{company.company_name}</Card.Header>
             <Card.Body>
-                <Card.Title>{company.email}</Card.Title>
-                <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-              
-                <ApplicationsModal company={company} jobSeeker={jobSeeker} loading={loading}/>
+                {/* <Card.Title><a href='' className='text-dark'>{post.title}</a></Card.Title> */}
+                {/* {console.log(post)} */}
+                <JobDescription  post={post} onToggle={() => onHandleToggle(post.id)} isOpen={open === post.id} company={company}/>
 
+                <div>
+                  {parse(DOMPurify.sanitize(post.jobDescription.slice(0, 300)+'...')) }
+                </div>
+                <ApplicationsModal post={post} company={company} jobSeeker={jobSeeker} loading={loading}/>
             </Card.Body>
-            <Card.Footer className="text-muted">Job Posted {formatDistanceToNow(parseISO(company.created_at))} ago</Card.Footer>
-            </Card>
-          )
-      })}
-    </>
-    )
-  }
+            <Card.Footer className="text-muted">Job Posted {formatDistanceToNow(parseISO(post.created_at))} ago</Card.Footer>
+          </Card>
+        )
+      })))
+
+  if (!jobSeeker || jobSeeker.length === 0) {
+    return (
+      <div className="text-center">
+          <div className="spinner-border" role="status">
+            <span className="sr-only"></span>
+          </div>
+      </div>);
+  } else {
+    return (
+      <>
+        {companyPost}
+      </>
+      )
+    }
 }
 
 export default JobCard;
