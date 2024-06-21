68b009d1e13c758b1f295b1115ae6d98b2fa85b8
joseph mwanzia <makamwanzia@gmail.com>
2024-06-21 23:51:54 +0300

    post model associations and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
-  
+  validates :title, :department, :jobDescription, :responsibilities, :qualifications, :experience, :location, :jobType, :remote, presence: true
   belongs_to :company
+  # belongs_to :application
+  has_many :applications
+  has_many :job_seekers, through: :applications
 end
