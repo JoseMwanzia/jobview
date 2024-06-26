cbea7652ba2c2bc1a6cff6ac0dac3c4042b24289
joseph mwanzia <makamwanzia@gmail.com>
2024-06-26 16:37:24 +0300

    jobseeker dashboard refactored

--- client/src/componets/jobSeeker/JobSeekerDashboard.js
+++ client/src/componets/jobSeeker/JobSeekerDashboard.js
@@ -1,17 +1,8 @@
 import React, {useState} from 'react'
-import logo from '../../assets/imgLogo.png';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import { Link } from 'react-router-dom';
 import JobCard from './JobCard';
-import Logout from '../LoginAndLogout/Logout';
+
+import Navigation from './Navigation';
 
 
 
@@ -25,73 +16,7 @@ function JobSeekerDashboard({jobSeeker, companyUser, loading}) {
 
   return (
   <>
-    <Navbar expand="lg" collapseOnSelect sticky='top' className="bg-body-tertiary">
-      <Container fluid style={{width: "100%"}}>
-
-      
-        <Col xs={6} md={4}>
-          <Link to='/companyDashboard'>
-            <Image src={logo} rounded style={{width: "25%", aspectRatio: "3/2", objectFit: 'contain'}} />
-          </Link>
-          <div className='me-5'>Welcome, {jobSeeker.first_name}</div>
-        </Col>
-
-        <Navbar.Toggle aria-controls="navbarScroll" />
-
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Find Jobs</Nav.Link>
-
-            <NavDropdown title="Employers" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#find-talent">Find Talent</NavDropdown.Item>
-              <NavDropdown.Item href="#more-talent">
-                More Talent
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#more-talent2">
-              More Talent
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Candidates" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Job Listings</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-              More Talent
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action5">
-                Job Listings
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Pages" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-            <Logout jobSeeker={jobSeeker}/>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
+    <Navigation jobSeeker={jobSeeker}/>
     <JobCard open={open} onHandleToggle={handleToggle} jobSeeker={jobSeeker} companyUser={companyUser} loading={loading}/>
   </>
   )
