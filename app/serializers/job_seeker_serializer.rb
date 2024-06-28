a35dbe757bb5d017ea92ba37cb4499a175de590e
joseph mwanzia <makamwanzia@gmail.com>
2024-06-28 23:46:46 +0300

    image_url serializer

--- app/serializers/job_seeker_serializer.rb
+++ app/serializers/job_seeker_serializer.rb
@@ -1,5 +1,5 @@
 class JobSeekerSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :created_at
+  attributes :id, :first_name, :last_name, :email, :created_at, :image_url
 
   # has_many :applications
   has_many :companies, through: :applications
