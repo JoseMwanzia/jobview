301127c8cc7547724f99d195a03b19d56eb8ff2c
joseph mwanzia <makamwanzia@gmail.com>
2024-07-01 23:31:19 +0300

    dependent association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :title, :department, :jobDescription, :responsibilities, :qualifications, :experience, :location, :jobType, :remote, presence: true
   belongs_to :company
   # belongs_to :application
-  has_many :applications
+  has_many :applications, dependent: :destroy
   has_many :job_seekers, through: :applications
 end
