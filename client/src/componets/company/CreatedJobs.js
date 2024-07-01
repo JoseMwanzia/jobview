0ecc235ca05070a62eadd18c6980ad09a0fe51c1
joseph mwanzia <makamwanzia@gmail.com>
2024-07-01 23:37:11 +0300

    confirmation component child

--- client/src/componets/company/CreatedJobs.js
+++ client/src/componets/company/CreatedJobs.js
@@ -6,6 +6,7 @@ import JobDetails from './JobDetails';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Applicants from './Applicants';
+import ConfrimationPop from './ConfirmationPop';
 
 function CreatedJobs({myCompany}) {
 
@@ -27,7 +28,7 @@ function CreatedJobs({myCompany}) {
 
   function handleDelete(post_id) {
     fetch(`/posts/${post_id}`, {
-        method: 'DELETE'
+      method: 'DELETE'
     })
   }
 
@@ -48,11 +49,7 @@ function CreatedJobs({myCompany}) {
                 {/* <Button as={Link} to={{pathname: '/applicants', state: {post}}} variant='outline-primary' className='m-1'>See Applicants</Button> */}
                 <Applicants myCompany={myCompany} post={post} show={show} setShow={setShow} fullscreen={fullscreen} onHandleShow={handleShow}/>
 
-                <Button onClick={() => handleDelete(post.id)} variant='outline-danger' className='m-1 delete-button'>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" className="bi bi-trash3 delete-icon" viewBox="0 0 16 16">
-                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                  </svg>
-                </Button>
+                <ConfrimationPop handledelete={()=>handleDelete(post.id)} close={close}/>
             </Card.Body>
             </Card>
         )
