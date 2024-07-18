0fdb0bffe3712c04229b411af9640d4c6a872c53
joseph mwanzia <makamwanzia@gmail.com>
2024-07-19 02:32:12 +0300

    company model spec test

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'before being saved' do
+    it 'has secure password' do
+      company = Company.create!(company_name:'Acme Corp', email: 'mail@gmail.com', password: 'password123', password_confirmation: 'password123')
+      expect(company).to be_valid
+      expect(company.password_digest).not_to be_nil
+    end
+  end
 end
