767303bbd31ef1aa34c50bb0dc080bd0dd301be6
joseph mwanzia <makamwanzia@gmail.com>
2024-07-19 02:33:27 +0300

    company request spec test

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Companies", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:company) { FactoryBot.create(:company) }
+
+  before do
+    allow_any_instance_of(CompaniesController).to receive(:authorize).and_return(true)
+  end
+
+  describe "GET /companies" do
+    it 'returns a success respose' do
+      get companies_path
+      puts response.body
+      expect(response).to be_successful
+
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response[0]['company_name']).to eq(company.company_name)
+    end
   end
 end
