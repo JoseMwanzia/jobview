93ab13e2e7dafd922f9877eeb86889b38b0282df
joseph mwanzia <makamwanzia@gmail.com>
2024-07-23 23:55:24 +0300

    code refactor

--- client/src/componets/jobSeeker/AppliedJobs.js
+++ client/src/componets/jobSeeker/AppliedJobs.js
@@ -28,10 +28,10 @@ function AppliedJobs({post,isOpen, onToggle, company}) {
           
           <h1 className='text-decoration-underline d-inline'>{post.title}</h1>
           {/* <h5 id='example-collapse-text'>at {company.company_name}</h5> */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" className="d-inline bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+          {/* <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" className="d-inline bi bi-box-arrow-up-right" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
             <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
-          </svg>
+          </svg> */}
           <br/>
           
           <h3 id='example-collapse-text'>Department: {post.department}</h3>
